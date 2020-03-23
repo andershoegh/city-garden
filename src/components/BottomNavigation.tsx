c1f6fbcd9cd264189e7a2304416e7dde9a9b1a03
Anders Høgh <ahha15@student.aau.dk>
2020-03-23 13:24:05 +0100

    Added bottomnavigation + routing

---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,44 @@
+import {
+  IonTabs,
+  IonRouterOutlet,
+  IonLabel,
+  IonTabButton,
+  IonTabBar,
+  IonIcon,
+  IonPage,
+  IonApp
+} from "@ionic/react";
+import React from "react";
+import { Redirect, Route } from "react-router";
+import Home from "../pages/Home";
+import { leafOutline, clipboardOutline } from "ionicons/icons";
+import { IonReactRouter } from "@ionic/react-router";
+
+interface BottomNavigationProps {}
+
+export const BottomNavigation: React.FC<BottomNavigationProps> = () => {
+  return (
+    <IonApp>
+      <IonPage>
+        <IonReactRouter>
+          <Redirect path="/" to="/have"></Redirect>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route path="/have" component={Home} exact />
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="have" href="/home">
+                <IonIcon icon={leafOutline}></IonIcon>
+                <IonLabel>Have</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="opslagstavle" href="/opslagstavle">
+                <IonIcon icon={clipboardOutline} />
+                <IonLabel>Opslagstavle</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+      </IonPage>
+    </IonApp>
+  );
+};
