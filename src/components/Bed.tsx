eaab99293727e15941e4580dc71abcf43bef3b61
Joachim <joachim.klyssing@gmail.com>
2020-03-23 23:18:07 +0100

    Tilføjet bed komponent
    Oprettet filen, linket den med css og tjekket den kunne kaldes. Har leget lidt med props.

---
+++ src/components/Bed.tsx
@@ -0,0 +1,24 @@
+import { IonPage } from "@ionic/react";
+import React from "react";
+import "./Bed.css";
+
+interface BedProps {
+    bedNr:number,
+    indhold?:string,
+    info?:string,
+    opgave?:string,
+    taget?:boolean,
+    style:string,
+}
+
+
+
+const Bed: React.FC<BedProps> = ({bedNr,indhold,info,opgave,taget,style}) => {
+    let className = 'bed-style';
+    if(style) className += '-'+style;
+    return (
+        <button className={className} onClick={()=>(console.log(style,'\n',bedNr))}>Click ME!</button>
+    );
+};
+
+export default Bed;
