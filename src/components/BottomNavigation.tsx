317516101d7f609cf1b28bb9e899eee75e6ac6cb
Anders Høgh <ahha15@student.aau.dk>
2020-04-22 12:13:57 +0200

    Small fixes before presentation

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -6,16 +6,17 @@ import {
   IonTabBar,
   IonIcon,
   IonPage,
-  IonApp
-} from '@ionic/react';
-import React from 'react';
-import { Redirect, Route } from 'react-router';
-import Home from '../pages/Home';
-import { leafOutline, clipboardOutline, calendarOutline } from 'ionicons/icons';
-import { IonReactRouter } from '@ionic/react-router';
-import MessageBoard from '../pages/MessageBoard';
-import Dummy from '../pages/Dummy';
-import Events from '../pages/Events';
+  IonApp,
+  IonRippleEffect,
+} from "@ionic/react";
+import React from "react";
+import { Redirect, Route } from "react-router";
+import Home from "../pages/Home";
+import { leafOutline, clipboardOutline, calendarOutline } from "ionicons/icons";
+import { IonReactRouter } from "@ionic/react-router";
+import MessageBoard from "../pages/MessageBoard";
+import Dummy from "../pages/Dummy";
+import Events from "../pages/Events";
 
 interface BottomNavigationProps {}
 
@@ -26,24 +27,27 @@ export const BottomNavigation: React.FC<BottomNavigationProps> = () => {
         <IonReactRouter>
           <IonTabs>
             <IonRouterOutlet>
-              <Route path='/garden' component={Home} exact />
-              <Route path='/messageboard' component={MessageBoard} exact />
-              <Route path='/events' component={Events} />
-              <Route path='/dummy' component={Dummy} />
-              <Redirect path='/' to='/garden' exact />
+              <Route path="/garden" component={Home} exact />
+              <Route path="/messageboard" component={MessageBoard} exact />
+              <Route path="/events" component={Events} />
+              <Route path="/dummy" component={Dummy} />
+              <Redirect path="/" to="/garden" exact />
             </IonRouterOutlet>
-            <IonTabBar slot='bottom'>
-              <IonTabButton tab='garden' href='/garden'>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="garden" href="/garden">
                 <IonIcon icon={leafOutline}></IonIcon>
                 <IonLabel>Garden</IonLabel>
+                <IonRippleEffect />
               </IonTabButton>
-              <IonTabButton tab='messageboard' href='/messageboard'>
+              <IonTabButton tab="messageboard" href="/messageboard">
                 <IonIcon icon={clipboardOutline} />
                 <IonLabel>Message board</IonLabel>
+                <IonRippleEffect />
               </IonTabButton>
-              <IonTabButton tab='events' href='/events'>
+              <IonTabButton tab="events" href="/events">
                 <IonIcon icon={calendarOutline} />
                 <IonLabel>Events</IonLabel>
+                <IonRippleEffect />
               </IonTabButton>
             </IonTabBar>
           </IonTabs>
