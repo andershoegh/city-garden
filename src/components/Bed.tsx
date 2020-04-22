27bc461ffb8957c7b3cf83abf80415edce5f4abc
Anders Høgh <ahha15@student.aau.dk>
2020-04-22 11:40:52 +0200

    Removed old code

--- src/components/Bed.tsx
+++ src/components/Bed.tsx
@@ -1,7 +1,7 @@
-import { IonButton, IonAlert, IonBadge } from '@ionic/react';
-import React, { useState, useEffect } from 'react';
-import './Bed.css';
-import { firebase } from '../Utility/Firebase';
+import { IonButton, IonAlert, IonBadge } from "@ionic/react";
+import React, { useState, useEffect } from "react";
+import "./Bed.css";
+import { firebase } from "../Utility/Firebase";
 
 interface BedProps {
   bedNr: string;
@@ -13,135 +13,144 @@ interface BedProps {
   availTasks: number;
 }
 
-const Bed: React.FC<BedProps> = props => {
-  const { bedNr, content, airMoisture, soilMoisture, soilTemperature, setSelection, availTasks } = props;
+const Bed: React.FC<BedProps> = (props) => {
+  const {
+    bedNr,
+    content,
+    airMoisture,
+    soilMoisture,
+    soilTemperature,
+    setSelection,
+    availTasks,
+  } = props;
   const [showEmptyAlert, setShowEmptyAlert] = useState(false);
   const [showFullAlert, setShowFullAlert] = useState(false);
-  let boxIllustration = content != null ? 'gardenbox-' + content + ' gardenbox' : 'gardenbox-empty gardenbox';
+  let boxIllustration =
+    content != null
+      ? "gardenbox-" + content + " gardenbox"
+      : "gardenbox-empty gardenbox";
   const DOUBLE_CLICK_THRESHOLD = 300;
   const [touchStart, setTouchStart] = useState<number>(0);
-  let message = 'This garden box contains ' + content + '. <br> Click on okay to empty garden box ' + bedNr + '.';
+  let message =
+    "This garden box contains " +
+    content +
+    ". <br> Click on okay to empty garden box " +
+    bedNr +
+    ".";
 
   const touchRelease = () => {
     const touchEnd = Date.now();
 
     if (Math.abs(touchEnd - touchStart) >= DOUBLE_CLICK_THRESHOLD) {
-      if (content !== 'empty') { setShowFullAlert(true) }
+      if (content !== "empty") {
+        setShowFullAlert(true);
+      }
       setTouchStart(0);
     } else {
       setSelection(bedNr);
-      const taskElement: any = document.getElementById(bedNr + '-tasks-id');
+      const taskElement: any = document.getElementById(bedNr + "-tasks-id");
       if (taskElement !== null) {
-        taskElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' });
+        taskElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "center",
+        });
       }
-      if (content === 'empty') { setShowEmptyAlert(true) }
-      
+      if (content === "empty") {
+        setShowEmptyAlert(true);
+      }
+
       setTouchStart(0);
     }
-  }
-
-  // useEffect(() => {
-  //   firebase.db.collection('plants').doc(content).onSnapshot(snapshot => {
-  //   if(soilMoisture > snapshot.get('soilMoisture')){
-  //     console.log('useEffect with soil')
-  //     firebase.db.collection('alltasks').where('gardenBoxId','==',bedNr).where('taskTemplateId','==','watering').onSnapshot(snapShot => {
-  //       if(snapShot.size === 0){
-  //         firebase.db
-  //           .collection("alltasks")
-  //           .doc()
-  //           .set({
-  //             created: firebase.firestore.Timestamp.fromDate(new Date()),
-  //             finished: false,
-  //             gardenBoxId:bedNr,
-  //             taskTaken: false,
-  //             taskTemplateId:'watering'
-  //           })
-  //           .then(function() {
-  //             console.log("Document in Firebase = OK!");
-  //           });
-  //       }else{
-  //         console.log('fandt intet');
-  //       }
-  //     })
-  //   }
-  // })
-  // }, [soilMoisture]);
+  };
 
   return (
     <div>
-    <IonButton
-      className={boxIllustration}
-      fill='clear'
-      expand='block'
-      onTouchStart={() => setTouchStart(Date.now())}
-      onTouchEnd={() => touchRelease()}
-    >
-      {bedNr}
-      <IonAlert
+      <IonButton
+        className={boxIllustration}
+        fill="clear"
+        expand="block"
+        onTouchStart={() => setTouchStart(Date.now())}
+        onTouchEnd={() => touchRelease()}
+      >
+        {bedNr}
+        <IonAlert
           isOpen={showEmptyAlert}
           onDidDismiss={() => setShowEmptyAlert(false)}
-          header={'Plant'}
-          subHeader={'Do you want to plant something?'}
-          message={'Please select what to plant.'}
-          inputs={[{
-            name: 'plantSelection',
-            type: 'radio',
-            label: 'Peas',
-            value: 'peas',
-            checked: true},
-            {name: 'plantSelection',
-            type: 'radio',
-            label: 'Carrots',
-            value: 'carrots'},
-            {name: 'plantSelection',
-            type: 'radio',
-            label: 'Squash',
-            value: 'squash'},
-            {name: 'plantSelection',
-            type: 'radio',
-            label: 'Potatoes',
-            value: 'potatoes'},
-            {name: 'plantSelection',
-            type: 'radio',
-            label: 'Lettuce',
-            value: 'lettuce'}
+          header={"Plant"}
+          subHeader={"Do you want to plant something?"}
+          message={"Please select what to plant."}
+          inputs={[
+            {
+              name: "plantSelection",
+              type: "radio",
+              label: "Peas",
+              value: "peas",
+              checked: true,
+            },
+            {
+              name: "plantSelection",
+              type: "radio",
+              label: "Carrots",
+              value: "carrots",
+            },
+            {
+              name: "plantSelection",
+              type: "radio",
+              label: "Squash",
+              value: "squash",
+            },
+            {
+              name: "plantSelection",
+              type: "radio",
+              label: "Potatoes",
+              value: "potatoes",
+            },
+            {
+              name: "plantSelection",
+              type: "radio",
+              label: "Lettuce",
+              value: "lettuce",
+            },
           ]}
-          buttons={[{
-            text: 'Cancel',
-            role: 'cancel',
-            cssClass: 'alert-cancel'
-          },
-          {
-            text: 'Plant',
-            handler: e => {
-              firebase.updatePlant(bedNr, e)
-            }
-          }]}          
+          buttons={[
+            {
+              text: "Cancel",
+              role: "cancel",
+              cssClass: "alert-cancel",
+            },
+            {
+              text: "Plant",
+              handler: (e) => {
+                firebase.updatePlant(bedNr, e);
+              },
+            },
+          ]}
         />
-        <IonAlert 
+        <IonAlert
           isOpen={showFullAlert}
           onDidDismiss={() => setShowFullAlert(false)}
-          header='Empty'
-          subHeader='Do you want to empty this garden box?'
+          header="Empty"
+          subHeader="Do you want to empty this garden box?"
           message={message}
-          buttons={[{
-            text: 'Cancel',
-            role: 'cancel',
-            cssClass: 'alert.cancel'
-          },
-          {
-            text: 'Okay',
-            handler: e => {
-              firebase.updatePlant(bedNr, 'empty')
-            }
-          }
+          buttons={[
+            {
+              text: "Cancel",
+              role: "cancel",
+              cssClass: "alert.cancel",
+            },
+            {
+              text: "Okay",
+              handler: (e) => {
+                firebase.updatePlant(bedNr, "empty");
+              },
+            },
           ]}
         />
-    </IonButton>
-    { availTasks !== 0 ? 
-      (<IonBadge className='badge'> {availTasks} </IonBadge>) 
-      : (null) 
-    }
+      </IonButton>
+      {availTasks !== 0 ? (
+        <IonBadge className="badge"> {availTasks} </IonBadge>
+      ) : null}
     </div>
   );
 };
